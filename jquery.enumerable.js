ba5358e211ed7adfcb08610d5ddf74f41fbf65b8
Xavier Shay <xavier@rhnh.net>
2008-12-28 15:53:52 -0800

    Refactor, DRY up heaps

--- jquery.enumerable.js
+++ jquery.enumerable.js
@@ -1,59 +1,71 @@
 (function ( $ ) {
-  $.collect = function(enumerable, callback) {
-    validateCallback(callback);
+  var methods = {
+    collect: function(enumerable, callback) {
+      var result = [];
+      $.each(enumerable, function(index) {
+        result.push(callback.call(this, index));
+      });
+      return result;
+    },
 
-    var result = [];
-    $.each(enumerable, function (index) {
-      result.push(callback.call(this, index));
-    });
-    return result;
-  }
+    inject: function(enumerable, initialValue, callback) {
+      var accumulator = initialValue;
 
-  $.fn.collect = function(callback) {
-    return $.collect(this, callback);
-  }
+      $.each(enumerable, function (index) {
+        accumulator = callback.call(this, accumulator, index);
+      });
+      return accumulator;
+    },
 
-  $.inject = function(enumerable, initialValue, callback) {
-    validateCallback(callback);
+    select: function(enumerable, callback) {
+      var result = [];
+      $.each(enumerable, function(index) {
+        if (callback.call(this, index))
+          result.push(this);
+      });
+      return result;
+    },
 
-    var accumulator = initialValue;
+    sum: function(enumerable) {
+      return $.inject(enumerable, 0, function(accumulator) {
+        return accumulator + this;
+      });
+    }
+  };
 
-    $.each(enumerable, function (index) {
-      accumulator = callback.call(this, accumulator, index);
-    });
-    return accumulator;
-  }
-
-  $.fn.inject = function(initialValue, callback) {
-    return $.inject(this, initialValue, callback);
-  }
-
-  $.select = function(enumerable, callback) {
-    validateCallback(callback);
-
-    var result = [];
-    $.each(enumerable, function(index) {
-      if (callback.call(this, index))
-        result.push(this);
-    });
-    return result;
-  }
-
-  $.fn.select = function(callback) {
-    return $.select(this, callback);
-  }
-
-  $.sum = function(enumerable) {
-    return $.inject(enumerable, 0, function(accumulator) {
-      return accumulator + this;
-    });
-  }
-
-  $.fn.sum = function() {
-    return $.sum(this);
-  }
+  var staticFunctions = {};
+  var iteratorFunctions = {};
+  $.each( methods, function(name, f){ 
+    staticFunctions[name]   = makeStaticFunction(f);
+    iteratorFunctions[name] = makeIteratorFunction(staticFunctions[name]);
+  });
+  $.extend(staticFunctions);
+  $.fn.extend(iteratorFunctions);
 
   // Private methods
+  function makeStaticFunction(f) {
+    return function() {
+      if (arguments.length > 1) // The first argument is the enumerable
+        validateCallback(arguments[arguments.length - 1]);
+
+      return f.apply(this, arguments);
+    }
+  }
+
+  function makeIteratorFunction(staticFunction) {
+    return function() {
+      // arguments isn't a real array, concat doesn't work
+      // unless you explicitly convert it
+      function toArray() {
+        var result = []
+        for (var i = 0; i < this.length; i++)
+          result.push(this[i])
+        return(result)
+      }
+      return staticFunction.apply(this, [this].concat(toArray.apply(arguments)))
+    }
+  }
+
   function validateCallback(callback) {
     if (!jQuery.isFunction(callback))
       throw("callback needs to be a function, it was: " + callback);
