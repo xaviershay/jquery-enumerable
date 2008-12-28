966e077b87120811ca2eb322e0365ca4cb007da6
Xavier Shay <xavier@rhnh.net>
2008-12-27 17:46:09 -0800

    Simplify collect spec

--- spec/collect_spec.js
+++ spec/collect_spec.js
@@ -6,12 +6,8 @@ Screw.Unit(function() {
         return [this * this, index];
       } 
       var it_behaves_like_collect = function() {
-        it("yields each element and returns a new array containing the return value of that yield", function() {
-          expect([result[0][0], result[1][0], result[2][0]]).to(equal, [1, 4, 9]);
-        });
-
-        it("yields the current index to the callback function", function() {
-          expect([result[0][1], result[1][1], result[2][1]]).to(equal, [0, 1, 2]);
+        it("returns a new array with the results of passing the current index to the block, with the current element in this", function() {
+          expect(result).to(equal, [[1, 0], [4, 1], [9, 2]]);
         });
       }
 
